bc8d090e5db618eda36d235cf6f082b18d09c40b
LiangYue <LiangYue1981816@Hotmail.com>
2020-08-19 18:05:51 +0800

    + 添加不压缩支流分量

--- SHCompress/Main.cpp
+++ SHCompress/Main.cpp
@@ -294,6 +294,111 @@ void Test27(float percent)
 	return;
 }
 
+void Test27_DC(float percent)
+{
+	const int n = 8 * 3;
+	const int count = 3741 / 3;
+
+	float **data_dc = (float **)AllocMatrix(3, count, sizeof(float));
+	float **data_set = (float **)AllocMatrix(n, count, sizeof(float));
+
+	if (FILE *pFile = fopen("data.txt", "rb")) {
+		for (int index = 0; index < count; index++) {
+			for (int i = 0; i < 3; i++) {
+				float data[9] = { 0.0f };
+
+				for (int j = 0; j < 9; j++) {
+					fscanf(pFile, "%f", &data[j]);
+				}
+
+				data_dc[i][index] = data[0] / PI;
+
+				for (int j = 0; j < n / 3; j++) {
+					data_set[i * n / 3 + j][index] = data[j + 1] / PI;
+				}
+			}
+		}
+		fclose(pFile);
+	}
+	else {
+		FreeMatrix((void **)data_dc);
+		FreeMatrix((void **)data_set);
+		return;
+	}
+
+	SHData sh_data;
+	SHInit(&sh_data);
+	SHAlloc(&sh_data, n);
+	SHBuild(&sh_data, data_set, count, percent);
+
+	IMAGE imgSource;
+	IMAGE imgCompress;
+	IMAGE imgPreview;
+	IMAGE_ZeroImage(&imgSource);
+	IMAGE_ZeroImage(&imgCompress);
+	IMAGE_ZeroImage(&imgPreview);
+	IMAGE_AllocImage(&imgSource, size * 4, size * 3, 24);
+	IMAGE_AllocImage(&imgCompress, size * 4, size * 3, 24);
+	IMAGE_AllocImage(&imgPreview, size * 4, size * 3 * 2, 24);
+
+	for (int index = 0; index < count; index++) {
+		printf("Test27 %d/%d\n", index, count);
+
+		char szFileName[260];
+
+		float sh_0[n + 3] = { 0.0f };
+		float sh_1[n + 3] = { 0.0f };
+		float source[n] = { 0.0f };
+		float compress[n] = { 0.0f };
+
+		for (int i = 0; i < 3; i++) {
+			sh_0[i * (n + 3) / 3] = data_dc[i][index];
+
+			for (int j = 0; j < n / 3; j++) {
+				sh_0[i * (n + 3) / 3 + j + 1] = data_set[i * n / 3 + j][index];
+			}
+		}
+
+		Preview9(&imgSource, &sh_0[0], &sh_0[9], &sh_0[18], size);
+
+		for (int i = 0; i < n; i++) {
+			source[i] = data_set[i][index];
+		}
+
+		SHCompress(&sh_data, source, compress);
+		SHUncompress(&sh_data, compress, source);
+
+		for (int i = 0; i < 3; i++) {
+			sh_1[i * (n + 3) / 3] = data_dc[i][index];
+
+			for (int j = 0; j < n / 3; j++) {
+				sh_1[i * (n + 3) / 3 + j + 1] = source[i * n / 3 + j];
+			}
+		}
+
+		Preview9(&imgCompress, &sh_1[0], &sh_1[9], &sh_1[18], size);
+
+		IMAGE_SetImageArea(&imgPreview, 0, 0, IMAGE_WIDTH(&imgPreview) - 1, IMAGE_HEIGHT(&imgPreview) / 2 - 1);
+		IMAGE_CopyImageArea(&imgSource, &imgPreview);
+
+		IMAGE_SetImageArea(&imgPreview, 0, IMAGE_HEIGHT(&imgPreview) / 2, IMAGE_WIDTH(&imgPreview) - 1, IMAGE_HEIGHT(&imgPreview) - 1);
+		IMAGE_CopyImageArea(&imgCompress, &imgPreview);
+
+		sprintf(szFileName, "./Result/%d_3.jpg", index);
+		IMAGE_SaveJpg(szFileName, &imgPreview, 75);
+	}
+
+	IMAGE_FreeImage(&imgSource);
+	IMAGE_FreeImage(&imgCompress);
+	IMAGE_FreeImage(&imgPreview);
+
+	SHFree(&sh_data);
+	FreeMatrix((void **)data_dc);
+	FreeMatrix((void **)data_set);
+
+	return;
+}
+
 int main(int argc, char **argv)
 {
 	glutInit(&argc, argv);
@@ -305,9 +410,10 @@ int main(int argc, char **argv)
 	glewInit();
 
 //	Test12(0.9f);
-	Test12_DC(0.9f);
-
 //	Test27(0.9f);
+
+	Test12_DC(0.6f);
+	Test27_DC(0.8f);
 
 	return 0;
 }
